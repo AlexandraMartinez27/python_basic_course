c3d3951741853e7f340fe4d88d60439aa54cf636
AlexandraMartinez27 <agresa_13@hotmail.com>
2025-09-23 20:27:00 -0400

    my first while loop

---
+++ 15-whilelop.py
@@ -0,0 +1,9 @@
+#execute a block of code multiple time and there are often 
+#usefull in biulding interactive programs and games
+
+i = 1
+while i <= 5:
+    print("<3" * i)
+    i = i + 1
+
+print('done')

70e787ef32ce6cd27471d3e4d23ccfb00b7e48ac
AlexandraMartinez27 <agresa_13@hotmail.com>
2025-10-03 16:42:59 -0400

    Car game

---
+++ While_loops/cargame.py
@@ -0,0 +1,8 @@
+input('ingrese comando:')
+
+command = str( input("ingrese comando: "))
+
+while command.lower()  == "help" :
+    print("star - to start the car\n stop - to stop the car")
+else:
+   print('hola') 
